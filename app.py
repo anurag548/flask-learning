965d8347b6380f070e86f5b2cbd7e6aca1778b22
anurag <anuragnsalian786@gmail.com>
2022-06-08 23:29:12 +0530

    Updated features

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+# from email import message
+from flask import Flask
+from flask_restful import Api, Resource, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,20 +12,37 @@ def index():
 
 
 videos_put_args = reqparse.RequestParser()
-videos_put_args.add_argument("name", type=str,help="Name of the video")
-videos_put_args.add_argument("views", type=int,help="Views of the video")
-videos_put_args.add_argument("likes", type=int,help="Likes of the video")
+videos_put_args.add_argument("name", type=str,help="Name of the video required", location='form',required=True)
+videos_put_args.add_argument("views", type=int,help="Views of the video required", location='form',required=True)
+videos_put_args.add_argument("likes", type=int,help="Likes of the video required", location='form', required=True)
 
 videos ={}
 
+def abrtIfIdNotExist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id is not valid...")
+
+def abrtIfIdExist(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID...")
+
 class Video(Resource):
     def get(self, video_id):
+        abrtIfIdNotExist(video_id)
         return videos[video_id]
     
     def put(self, video_id):
+        abrtIfIdExist(video_id)
         args = videos_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
 
+    def delete(self, video_id):
+        abrtIfIdNotExist(video_id)
+        del videos[video_id]
+        return '', 204
+
+    
 api.add_resource(Video, "/video/<int:video_id>")
 
 
--- test.py
+++ test.py
@@ -3,7 +3,12 @@ import  requests
 
 BASE = "http://127.0.0.1:5000/"
 
+datToSend = [{"likes":"100","name":"Test0","views":"123456"},{"likes":"100","name":"Test1","views":"12345678"},{"likes":"1000","name":"Test2","views":"123456789"},{"likes":"10000","name":"Test3","views":"1234567890"}]
 
-
-respons = requests.put(BASE+"video/19",{"likes":"10"})
+for i in datToSend:
+    requests.get(BASE+"video/"+s)
+    # print(respons.json())
+    
+input()
+respons = requests.get(BASE+"video/1")
 print(respons.json())
